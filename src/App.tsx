944203ef05da4ba94fdac16187a36bffa8b11e3c
duchau <duchaunee@gmail.com>
2024-06-03 02:37:39 +0700

    add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,6 @@
-import { useEffect } from "react";
 import "./App.css";
 
-//OUTPUT:
-/**
- * start
- * end
- * XONG ROI NE
- */
-
-//VẪN BẮT ĐƯỢC onSuccess MẶC DÙ KHÔNG DÙNG AWAIT
 function App() {
-  useEffect(() => {
-    const run = async () => {
-      console.log("start");
-      resolve(() => {
-        console.log("XONG ROI NE");
-      });
-      console.log("end");
-    };
-    run();
-  }, []);
-
-  const resolve = (onSuccess: () => void) => {
-    fetch("https://jsonplaceholder.typicode.com/todos/1")
-      .then((response) => response.json())
-      .then(() => {
-        onSuccess();
-      });
-  };
   return <>DWCHAU</>;
 }
 
